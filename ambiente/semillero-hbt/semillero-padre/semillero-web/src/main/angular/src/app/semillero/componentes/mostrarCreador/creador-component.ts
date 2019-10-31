eb6a2090424507f9c81ead639fcb2970991b6ece
castesebas <juacastellanos@uniboyaca.edu.co>
2019-10-31 10:29:55 -0500

    se agregaron comentarios

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/mostrarCreador/creador-component.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/mostrarCreador/creador-component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+/**
+ * @description Componente mostrar creador, el cual contiene la informacion basica del usuario
+ * 
+ * @author Juan Sebastian Castellanos <dalvarez@heinsohn.com.co>
+ */
 @Component({
     selector: 'mostrar-creador',
     templateUrl: './creador-component.html',
